d6a154ea4b01e286a0c8200a910a4943ec2ef536
Junkov0 <devjuncloud@gmail.com>
2026-01-13 15:49:32 +0900

    feat: MapStruct 전역 설정(GlobalMapperConfig) 도입

---
+++ src/main/java/com/sprint/mission/discodeit/mapper/GlobalMapperConfig.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.mapper;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(
+    componentModel = "spring",                     // 스프링 빈으로 등록
+    unmappedTargetPolicy = ReportingPolicy.IGNORE // 매핑 되지 않는 필드 있어도 무시
+)
+public interface GlobalMapperConfig {
+
+}
