5b88a9154e91fff56f0ee847d8284620cbca3c0e
Junkov0 <devjuncloud@gmail.com>
2026-01-13 20:44:32 +0900

    feat: JPA Auditing을 위한 BaseEntity 및 BaseUpdatableEntity 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.sprint.mission.discodeit.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 프록시나 자식 클래스에서는 접근 가능하게 제한적으로 공개 여부를 조절한다.
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(updatable = false, nullable = false)
+  private UUID id;
+
+  @CreatedDate
+  @Column(updatable = false, nullable = false)
+  private Instant createdAt;
+}
