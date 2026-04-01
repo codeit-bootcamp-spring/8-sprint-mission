608c2ee9a21a93a9dc22afa2a7ed9e6860dcc990
idktomorrow <swj9675@naver.com>
2026-04-01 16:39:39 +0900

    세션을 활용한 현재 사용자 정보 조회 api 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,19 +2,16 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
 import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
-import com.sprint.mission.discodeit.service.AuthService;
-import jakarta.validation.Valid;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,7 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/auth")
 public class AuthController implements AuthApi {
 
-  private final AuthService authService;
 
   @GetMapping("csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
@@ -31,15 +27,14 @@ public class AuthController implements AuthApi {
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
-  @PostMapping(path = "login")
-  public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
-    log.info("로그인 요청: username={}", loginRequest.username());
+  @GetMapping("/me")
+  @Override
+  public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    if (userDetails == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 
-    UserDto user = authService.login(loginRequest);
-    log.debug("로그인 응답: {}", user);
-
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(user);
+    log.debug("현재 사용자 정보 조회: {}", userDetails.getUserDto().username());
+    return ResponseEntity.ok(userDetails.getUserDto());
   }
 }
