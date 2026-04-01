6b7059060a89640ddabfd1b8878ba59e544fba3a
seungowon00 <hsw62255@naver.com>
2026-04-01 18:48:55 +0900

    feat: Authentication - View current user information using sessions

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.config.auth.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.dto.data.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ public class AuthController {
 				String tokenValue = csrfToken.getToken();
 				log.debug("CSRF 토큰 요청: {}", tokenValue);
 				return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+		}
+
+		@GetMapping(path = "me")
+		public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+				return ResponseEntity.ok(userDetails.getUserDto());
 		}
 }
