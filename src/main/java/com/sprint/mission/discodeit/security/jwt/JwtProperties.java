d8cc0c3c0e98a4611f03b1ddeccdcf0acfee3272
seungowon00 <hsw62255@naver.com>
2026-04-12 22:38:33 +0900

    feat: JwtTokenProvider, JwtProperties 생성

---
+++ src/main/java/com/sprint/mission/discodeit/security/jwt/JwtProperties.java
@@ -0,0 +1,16 @@
+package com.sprint.mission.discodeit.security.jwt;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "discodeit.jwt")
+public record JwtProperties(
+        @NotBlank String secret,
+        @Positive long accessExpirationMs,
+        @Positive long refreshExpirationMs
+) {
+}
+
