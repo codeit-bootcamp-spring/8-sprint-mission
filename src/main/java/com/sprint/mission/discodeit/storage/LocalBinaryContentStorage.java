2262b3690a172197af883d66c70d2f66fe3c8fff
parkhamin <parkhamint@naver.com>
2026-04-16 15:15:12 +0900

    feat: 성능 모니터링을 위한 @Timed 어노테이션과 actuator 설정 추가

--- src/main/java/com/sprint/mission/discodeit/storage/LocalBinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/LocalBinaryContentStorage.java
@@ -48,6 +48,13 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
   public UUID put(UUID binaryContentId, byte[] bytes) {
     Path path = resolvePath(binaryContentId);
 
+    /*try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new RuntimeException("Thread interrupted while simulating delay", e);
+    }*/
+
     if (Files.exists(path)) {
       throw new IllegalArgumentException("해당 파일이 이미 존재합니다.");
     }
