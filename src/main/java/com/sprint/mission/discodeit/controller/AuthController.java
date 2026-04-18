1328c2942078bed3778b4cb132a1776f5a546a42
jaejo <jaejoon0520@naver.com>
2026-04-18 14:14:43 +0900

    fix: add missing sql

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.DTO.dto.JwtDTO;
-import com.sprint.mission.discodeit.DTO.dto.UserDto;
-import com.sprint.mission.discodeit.DTO.request.UserRoleUpdateRequest;
+import com.sprint.mission.discodeit.dto.dto.JwtDTO;
+import com.sprint.mission.discodeit.dto.dto.UserDto;
+import com.sprint.mission.discodeit.dto.request.UserRoleUpdateRequest;
 import com.sprint.mission.discodeit.DTO.response.ErrorResponse;
 import com.sprint.mission.discodeit.controller.api.AuthApi;
 import com.sprint.mission.discodeit.security.jwt.JwtTokenProvider;
