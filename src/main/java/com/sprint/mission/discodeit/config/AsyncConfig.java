1f3f56c90573749580207dff736699555d805922
idktomorrow <swj9675@naver.com>
2026-04-23 00:23:59 +0900

    비동기 실패 처리

--- src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -5,6 +5,7 @@ import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContext;
@@ -12,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableAsync
+@EnableRetry
 public class AsyncConfig {
 
   @Bean
