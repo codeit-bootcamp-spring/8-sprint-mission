284e98d1fb2e97754dc89465cf0feb61128ef81e
jaejo <jaejoon0520@naver.com>
2026-04-30 15:17:47 +0900

    fix: add @JsonCreator, @JsonProperty because no constructor exists

--- src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
+++ src/main/java/com/sprint/mission/discodeit/event/MessageCreatedEvent.java
@@ -1,12 +1,18 @@
 package com.sprint.mission.discodeit.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sprint.mission.discodeit.dto.dto.MessageDto;
 
 import java.time.Instant;
 
+// 
 public class MessageCreatedEvent extends CreatedEvent<MessageDto> {
-
-    public MessageCreatedEvent(MessageDto data, Instant createdAt) {
+    @JsonCreator
+    public MessageCreatedEvent(
+            @JsonProperty("data") MessageDto data,
+            @JsonProperty("createdAt") Instant createdAt
+    ) {
         super(data, createdAt);
     }
 }
